8ff3d0d22b9bc4e6a85182144f11e3a7f6fcccd6
ifnmg <ifnmg@localhost.localdomain>
2024-08-07 22:23:52 -0300

    Atualizando

--- src/models/filmeModel.js
+++ src/models/filmeModel.js
@@ -1,5 +1,5 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../config/dbCOnfig.js";
+import { DataTypes, DATE } from "sequelize";
+import sequelize from "../config/dbConfig.js";
 
 const filmeModel = sequelize.define('Filmes',{
     id:{
@@ -11,5 +11,22 @@ const filmeModel = sequelize.define('Filmes',{
     titulo: {
         type: DataTypes.STRING,
         allowNull: false,
+    },
+    diretor: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ano: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    genero: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
+},
+    {
+        timestamps:false
 });
+
+export default filmeModel;
